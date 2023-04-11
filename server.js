85935cdd3d24d515487a19a219c50f0471fd0709
Hirenkumar <hirenkumarht40@gmail.com>
2023-04-11 15:21:21 +0530

    Version 1.5

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ app.use('/api/document', documentRoutes);
 
 
 //static files
-app.use(express.static(path.resolve("./frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.resolve("./index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
 app.listen(PORT,()=>{
